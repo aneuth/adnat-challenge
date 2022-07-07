ad092e76da3444ce11b4b407777afa7814e8afe1
Anna Efthymiadou <aneuth@gmail.com>
2022-07-07 14:55:02 +0100

    added name and organoisation_id to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_07_132812) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_07_135236) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,9 +39,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_07_132812) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.bigint "organisation_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["organisation_id"], name: "index_users_on_organisation_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "shifts", "users"
+  add_foreign_key "users", "organisations"
 end
