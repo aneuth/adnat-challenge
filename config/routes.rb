6c15e6b137e4c42a29fff3968635959dd3d7629e
Anna Efthymiadou <aneuth@gmail.com>
2022-07-22 00:25:42 +0100

    added button to add breaks

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :organisations, only: [:create, :edit, :update] do
     get :join_organisation, to: "users#join_organisation", as: :join
     get :leave_organisation, to: "users#leave_organisation", as: :leave
-    resources :shifts, only: [:create, :update], as: :shifts
+    resources :shifts, only: [:create], as: :shifts
   end
 
   resources :shifts, only: [:index, :edit, :update, :destroy], as: :shifts
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
   get :prior_shifts, to: "shifts#view_prior", as: :view_prior
 
   get 'prior_shifts/:id/add_prior_shift', to: "shifts#add_prior_shift", as: :add_prior
+
+  resources :breaks, only: [:create]
 
   get :my_profile, to: "users#my_profile"
 end
