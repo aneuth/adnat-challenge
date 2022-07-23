aab8ca1e67ddf0182abe1583ee43f91d7b0ba115
Anna Efthymiadou <aneuth@gmail.com>
2022-07-23 02:10:03 +0100

    added unit test for break model

--- app/javascript/controllers/new_shift_controller.js
+++ app/javascript/controllers/new_shift_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="new-shift"
 export default class extends Controller {
 
-  static targets = ["form"]
+  static targets = ["form", "button"]
 
   connect() {
     console.log("You are connected to new-shift controller")
@@ -12,10 +12,12 @@ export default class extends Controller {
   revealContent(event) {
     event.preventDefault()
     this.formTarget.classList.remove("d-none")
+    this.buttonTarget.classList.add("d-none")
   }
 
   closeContent(event) {
     this.formTarget.classList.add("d-none")
+    this.buttonTarget.classList.remove("d-none")
   }
 
 }
--- test/models/break_test.rb
+++ test/models/break_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class BreakTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save a break without a break length" do
+    brk = Break.new
+    assert_not brk.save, "Saved break without a break length"
+  end
 end
