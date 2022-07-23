d8e04b23cbacfc012ed0371e7c551c78ab1c60eb
Anna Efthymiadou <aneuth@gmail.com>
2022-07-23 02:18:55 +0100

    added validaitons to organisaiton model for name uniqueness and hourly rate presence, added unit tests for organisation model

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,4 +1,7 @@
 class Organisation < ApplicationRecord
   has_many :users, dependent: :nullify
   has_many :shifts
+
+  validates :name, uniqueness: { case_sensitive: false }
+  validates :hourly_rate, presence: true
 end
--- test/models/organisation_test.rb
+++ test/models/organisation_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class OrganisationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not allow duplicate organisation names" do
+    org1 = Organisation.create(name: "Burger King")
+    org2 = Organisation.create(name: "Burger King")
+    org1.save
+    assert_not org2.save, "Saved a duplicate organisation name"
+  end
+
+  test "should not save an organisation without an hourly rate" do
+    org = Organisation.new
+    assert_not org.save, "Saved organisation without an hourly rate"
+  end
+
 end
